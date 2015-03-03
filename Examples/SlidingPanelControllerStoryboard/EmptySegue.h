eaf114d90ad4701d66cb0639dded6dcce86d16d6
Sébastien MICHOY <sebastienmichoy@gmail.com>
2015-03-04 00:24:19 +0100

    Update license headers and LICENSE file.

--- Examples/SlidingPanelControllerStoryboard/EmptySegue.h
+++ Examples/SlidingPanelControllerStoryboard/EmptySegue.h
@@ -1,6 +1,6 @@
 //  EmptySegue.h
 //
-// Copyright (c) 2014 Sébastien MICHOY
+// Copyright © 2014-2015 Sebastien MICHOY and contributors.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
