ac756c1ed8bfca2514f85e5e4b5f7c3463a4a247
Sébastien MICHOY <sebastienmichoy@gmail.com>
2016-02-27 19:35:09 +0100

    Updates unit tests to be more independent of the example target.

--- Examples/UnitTests/StoryboardInitializationTests.swift
+++ Examples/UnitTests/StoryboardInitializationTests.swift
@@ -27,6 +27,7 @@
 //  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 //  POSSIBILITY OF SUCH DAMAGE.
 
+import SlidingPanelExamples_Programmatically
 import XCTest
 
 class StoryboardInitializationTests: XCTestCase {
